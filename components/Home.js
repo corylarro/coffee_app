31b694825adb669c24780311010cb439bbef72eb
Cory Larro <cory.larro@gmail.com>
2021-10-17 14:18:57 -0700

    menu items

---
+++ components/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, Text, Animated } from 'react-native';
+import { Card } from 'react-native-elements';
+
+class Home extends Component {
+    
+    static navigationOptions = {
+        title: 'Home'
+    }
+
+    render() {
+        return (
+
+        );
+    }
+}
---
+++ shared/menu.js
@@ -0,0 +1,51 @@
+export const MENU = [
+    {
+        id: 0,
+        name: "Latte",
+        price: 4.99,
+        featured: true,
+        description: "Shot of espresso, steamed milk and topped with a layer of foamed milk."
+    },
+    {
+        id: 1,
+        name: "Black Coffee",
+        price: 2.49,
+        featured: false,
+        description: "Your favorite coffee shop's regular ol' black coffee."
+    },
+    {
+        id: 2,
+        name: "Hot Chocolate",
+        price: 3.49,
+        featured: false,
+        description: "Our rich and decadent hot chocolate is always a pleaser."
+    },
+    {
+        id: 3,
+        name: "Black Tea",
+        price: 3.49,
+        featured: false,
+        description: "A great alternative to coffee!"
+    },
+    {
+        id: 4,
+        name: "Coffee Cake",
+        price: 3.00,
+        featured: false,
+        description: "A rich, buttery cake topped with cinnamon crumbles."
+    },
+    {
+        id: 5, 
+        name: "Croissant",
+        price: 2.49,
+        featured: false,
+        description: "The next best thing to being in Paris."
+    },
+    {
+        id: 6,
+        name: "Assorted Muffins",
+        price: 3.00,
+        featured: false,
+        description: "Your choice of assorted muffins. Always a staple."
+    }
+];
